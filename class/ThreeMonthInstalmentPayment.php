e2cc03c99c49f4607ff36abb889cde16d6ea1b67
ro <sauvagementjaune@gmail.com>
2018-12-19 14:54:29 +0100

    3

--- class/ThreeMonthInstalmentPayment.php
+++ class/ThreeMonthInstalmentPayment.php
@@ -2,5 +2,8 @@
 class ThreeMonthInstalmentPayment implements IPayment
 {
 	public function totalAmount();
+	// creer une fonction qui done le resultat dans une varaible $total
 	public function paymentDetails();
+	// fonction qui prends le paiment ne 3 fois
+	
 }
